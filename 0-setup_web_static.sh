fc8d44924bbc6101b74de4d8518ae957c4f52e67
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-01-23 15:23:47 +0300

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,52 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
 
-apt-get update
-apt-get install -y nginx
+# Check if Nginx is installed
+if ! which nginx > /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create necessary directories
+if [ ! -d "/data" ]; then
+    sudo mkdir /data
+fi
+if [ ! -d "/data/web_static" ]; then
+    sudo mkdir /data/web_static
+fi
+if [ ! -d "/data/web_static/releases" ]; then
+    sudo mkdir /data/web_static/releases
+fi
+if [ ! -d "/data/web_static/shared" ]; then
+    sudo mkdir /data/web_static/shared
+fi
+if [ ! -d "/data/web_static/releases/test" ]; then
+    sudo mkdir /data/web_static/releases/test
+fi
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create a fake index.html file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+# Remove existing symbolic link, if any
+if [ -L "/data/web_static/current" ]; then
+    sudo rm /data/web_static/current
+fi
 
-service nginx restart
+# Create a new symbolic link to the test release
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data
+
+# Update Nginx configuration
+sudo sed -i 's/root \/var\/www\/html;/root \/data\/web_static\/current;/g' /etc/nginx/sites-available/default
+sudo sed -i 's/# location \/ {/location \/hbnb_static\/ {/g' /etc/nginx/sites-available/default
+sudo sed -i 's/#     alias \/usr\/share\/nginx\/html;/    alias \/data\/web_static\/current;/g' /etc/nginx/sites-available/default
+sudo service nginx restart
+
+exit 0
